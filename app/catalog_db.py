cffd8c4c7f8ee5bcb063ba68bcb8a98f86312ba0
Jun C <kjstudios858@gmail.com>
2018-03-14 13:23:27 -0700

    refactor hard code database URI

--- app/catalog_db.py
+++ app/catalog_db.py
@@ -6,7 +6,8 @@ from models import Base
 
 # engine = create_engine('sqlite:///catalogProject.db')
 # engine = create_engine('postgresql://student:student@localhost/catalog000')
-engine = create_engine(app.config['DATABASE_URI'])
+# engine = create_engine(app.config['DATABASE_URI'])
+engine = create_engine('postgresql://catalogowner:cat000@localhost/catalog')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
