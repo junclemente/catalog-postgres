bf64e099228c5d4cb25bf69aaa409e07f9aeed1c
Jun C <kjstudios858@gmail.com>
2018-03-14 13:43:38 -0700

    refactor testing configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,17 @@
 from flask import Flask
 
 app = Flask(__name__)
-#app.config.from_object('config')
+app.config.from_object('config')
 
 # Python module was created to better organize code
 from app import views, models, restful_api, authenticate, forms, catalog_db
 
 #import views
 
-@app.route("/")
-@app.route("/test")
-def test_index():
-    return "<h1>Hello World!</h1> Test Index! Catalog on Lightsail Amazon AAWS."
+# @app.route("/")
+# @app.route("/test")
+# def test_index():
+#     return "<h1>Hello World!</h1> Test Index! Catalog on Lightsail Amazon AAWS."
 
 #if __name__ == "__main__":
 #    app.run()
